12cfee38fe85b47b724c7c3253b1afd8caa1c71b
wangyafei <w7297911@163.com>
2018-07-08 22:28:07 +0800

    增加脚本文件

---
+++ startorder.sh
@@ -0,0 +1,12 @@
+﻿#!/usr/bin/env bash
+mvn clean install
+cd ~/apache-tomcat-9.0.10/bin
+sh shutdown.sh
+cd ../webapps/
+rm -rf /root/apache-tomcat-9.0.10/webapps/ROOT
+rm -f /root/apache-tomcat-9.0.10/webapps/ROOT.war
+rm -f /root/apache-tomcat-9.0.10/webapps/order.war
+cp /home/localhost/.jenkins/workspace/DeployOrder/order/target/order.war ~/apache-tomcat-9.0.10/webapps/
+mv order.war ROOT.war
+cd ../bin
+sh startup.sh
